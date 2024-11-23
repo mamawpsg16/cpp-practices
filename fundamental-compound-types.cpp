f9a5d2f7064713e41124223b53e93a59176e790e
Kevin <kevinmensah114@gmail.com>
2024-11-23 20:59:31 +0800

    Update

---
+++ fundamental-compound-types.cpp
@@ -0,0 +1,55 @@
+/** FUNDAMENTAL TYPES - These are the basic building blocks of data types in C++. They represent the most elementary types provided by the language. Fundamental types are also sometimes called primitive types. */
+// Integer Types:
+
+// int
+// short
+// long
+// long long
+// unsigned variations (e.g., unsigned int)
+// Floating-Point Types:
+
+// float
+// double
+// long double
+// Character Types:
+
+// char
+// unsigned char
+// signed char
+// wchar_t (wide character)
+// char8_t (UTF-8 character, added in C++20)
+// char16_t, char32_t (UTF-16 and UTF-32 characters)
+// Boolean Type:
+
+// bool
+// Void Type:
+
+// void (indicates "no type")
+
+
+/** COMPOUND TYPES - These are types constructed from fundamental types or other compound types. Compound types allow for more complex structures and behaviors.*/
+// Pointers:
+
+// int* ptr; // Points to an integer
+// References:
+
+// int& ref = age; // Reference to an integer
+// Arrays:
+
+// int arr[10]; // Array of 10 integers
+// Functions:
+
+// int add(int a, int b); // Function declaration
+// Enumerations (enum):
+
+// enum Color { Red, Green, Blue }; // Enum with named constants
+// Classes/Structs:
+
+// struct Point { int x, y; }; // Struct with two integer fields
+// class Circle { double radius; }; // Class with a double field
+// Unions:
+
+// union Data { int i; float f; }; // Union sharing memory between int and float
+// std::string:
+
+// std::string name = "Alice"; // String class representing a sequence of characters
